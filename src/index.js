78bffbb604eed0d7cfb5c776ef1bb87d1f3a3b47
Laura-Cos3 <cauraloombes@gmail.com>
2025-03-29 21:53:48 +0000

    Poem Form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La vie en rose",
+    autoStart: true,
+    delay: 1,
+    curtsor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
